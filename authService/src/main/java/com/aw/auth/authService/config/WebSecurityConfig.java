2112e61983bcc739c0bb22ae46d793e97ce90118
Amol Wasu <wasuamol@gmail.com>
2020-06-04 14:59:41 +0530

    test

--- authService/src/main/java/com/aw/auth/authService/config/WebSecurityConfig.java
+++ authService/src/main/java/com/aw/auth/authService/config/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	//dfsdf
 
 	@Autowired
 	private UserDetailsService jwtUserDetailsService;
